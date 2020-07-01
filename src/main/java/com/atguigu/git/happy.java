4d6e0049f716e0116c15caf59c1c33534476e3c4
xh <741423194@qq.com>
2020-07-01 22:16:42 +0800

    分支合并

--- src/main/java/com/atguigu/git/happy.java
+++ src/main/java/com/atguigu/git/happy.java
@@ -2,14 +2,13 @@ package com.atguigu.git;
 
 public class happy {
 	public static void main(String[] args){
-<<<<<<< HEAD
+
 		System.out.print("你好");
 		System.out.print("你好呀，111");
 		System.out.print("你好呀");
-=======
 		System.out.print("你好");
 		System.out.print("你好呀");
 		System.out.print("版本修复");
->>>>>>> refs/heads/hot_fix
+
 	}
 }
